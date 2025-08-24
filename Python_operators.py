12fc61d64c54e1a2e7a470bea5dc10756c4be524
amishanakoti143 <amunakoti143@gmail.com>
2025-08-24 23:55:43 +0530

    Update Python_operators.py

--- Python_operators.py
+++ Python_operators.py
@@ -4,7 +4,7 @@ Author: Amisha Nakoti
 
 """
 
-# ---------------------------
+# -------------------------
 # What is an Operator?
 # ---------------------------
 # Operators are symbols that perform operations on variables and values.
@@ -104,5 +104,6 @@ print("a is c:", a is not c)   # True
 x = [1, 2, 3, 4, 5]
 print("3 in nums:", 3 in x)
 print("6 not in nums:", 6 not in x)
+
 
 
