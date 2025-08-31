fc19d9e494f85376bd394cd71a66efbaae72eccc
amishanakoti143 <amunakoti143@gmail.com>
2025-08-31 23:43:20 +0530

    Update Python_Conditional_Statements.py

--- Python_Conditional_Statements.py
+++ Python_Conditional_Statements.py
@@ -118,5 +118,33 @@ else:
 
 '''Write a program to display month name based on month number (1-12)'''
 
+x =int(input("enter month no: "))
+if x==1:
+    print("january")
+elif x==2:
+    print("feb")
+elif x==3:
+    print("march")
+elif x==4:
+    print("april")
+elif x==5:
+    print("may")
+elif x==6:
+    print("june")
+elif x==7:
+    print("july")
+elif x==8:
+    print("aug")
+elif x==9:
+    print("sep")
+elif x==10:
+    print("oct")
+elif x==11:
+    print("nov")
+elif x==12:
+    print("dec")
+else:
+    print("error")    
+
 
 
