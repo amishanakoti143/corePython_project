bbc49cb02a9b4683495ed7084d5b0dfeb40ed8fc
Amisha Nakoti <amunakoti143@gmail.com>
2025-10-30 23:49:36 +0530

    Update Python_STRING.py

--- Python_STRING.py
+++ Python_STRING.py
@@ -275,12 +275,14 @@ x="asDFgH5jK345REiS"
 for i in x:
     if i.isupper():
         print(i)
+        
 #-----------------------------------------------------
 '''Print all elements which is written in small letter'''
 x="asDFgH5jK345REiS"
 for i in x:
     if i.islower():
         print(i)
+
 
 
 
