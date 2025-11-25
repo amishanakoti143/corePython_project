be7c430fa82abb6ba6c9f3a4bf715a95966bf6b5
Amisha Nakoti <amunakoti143@gmail.com>
2025-11-25 23:23:46 +0530

    Update Python_Conditional_Statements.py

--- Python_Conditional_Statements.py
+++ Python_Conditional_Statements.py
@@ -5,7 +5,7 @@ Author: Amisha Nakoti
 """
 
 # ---------------------------- 
-# What are Conditional Statements?
+# What are Conditional Statements? 
 # ---------------------------
 # Conditional statements are used to perform different actions based on different conditions.
 # Python uses if, elif, and else keywords for decision making.
@@ -203,6 +203,7 @@ for i in range(1, 21):
 x=int(input("Enter no-:   "))
 y=x%10
 print("this is the last digit:-",y)
+
 
 
 
