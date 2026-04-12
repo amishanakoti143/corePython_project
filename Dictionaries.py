bd9f8097df2635d648889d1af336b95c25a39142
Amisha Nakoti <amunakoti143@gmail.com>
2026-04-12 22:25:41 +0530

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -10,7 +10,7 @@
 3->  dict are mutable or changable .
 4->  dict are not indexed .   
 5->  dict not allow duplicate keys.
-6-.  it also support multiple typpe of Data.   
+6-.  it also support multiple typpe of Data...
 ________________________________________________________________________________________________________
  
 GET-->  its extract values base of Keys from dict.
